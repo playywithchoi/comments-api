6b299a7e2bf40f0671f51cf8e764842ff243b22d
yeonju <0751choi@gmail.com>
2024-12-12 17:08:45 +0900

    Fix eslint error

--- api/comments.js
+++ api/comments.js
@@ -1,10 +1,20 @@
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-if (req.method === 'OPTIONS') {
-  res.status(204).end();
-  return;
-}
+export default async function handler(req, res) {
+  // CORS 설정
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // OPTIONS 요청에 대한 빠른 응답
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection('comments');
+    // 기존 코드 유지
+
 import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI);
