a9dcbd52b197b5d3d0a6b61dcc2198bbe4ed73a4
yeonju <0751choi@gmail.com>
2024-12-18 15:07:02 +0900

    Fix eslint error

--- server/server.js
+++ server/server.js
@@ -1,26 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './utils/mongodb.js';
-import commentRoutes from './api/comments.js';
-import path from 'path'; // path 모듈 추가
-
-dotenv.config();
+import path from 'path'; // path 모듈
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(express.json());
+// client/public 폴더를 정적 파일 제공 경로로 설정
+app.use(express.static(path.join(__dirname, '../client/public')));
 
-// 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'public'))); // 'public' 폴더에서 정적 파일 제공
-
-// Routes
-app.use('/api/comments', commentRoutes);
-
-// Database Connection
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+// 서버 실행
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
